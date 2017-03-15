d6512c8a8061ea5078db45a355f2ed53c5c2b614
cbalamanatuic@gmail.com <clydeinwebdev@gmail.com>
2017-03-15 11:19:14 +0800

    setup

--- js/app.js
+++ js/app.js
@@ -2,4 +2,4 @@
 var app = document.querySelector("#app");
 app.innerHTML = '<h1>LOCKER APP</h1>';
 app.innerHTML += '<img src="img/decode.png" />';
-app.innerHTML += '<i class="material-icons">home</i>';
+app.innerHTML += '<i class="material-icons">&#xE87C;</i>';
