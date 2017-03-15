490e754911da167d8b10de397dc2e5c15599d606
cbalamanatuic@gmail.com <clydeinwebdev@gmail.com>
2017-03-15 11:09:06 +0800

    setup

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
 "use strict";
 var app = document.querySelector("#app");
 app.innerHTML = '<h1>LOCKER APP</h1>';
-app.innerHTML += '<img src="decode.png" />';
+app.innerHTML += '<img src="img/decode.png" />';
